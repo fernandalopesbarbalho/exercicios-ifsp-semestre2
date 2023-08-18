f2b6fb119174f20d8722799c785c97cb2a9d8d0e
Fernanda Lopes Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2023-08-18 13:35:38 -0300

    Update Main.java

--- Carros/Main.java
+++ Carros/Main.java
@@ -1,3 +1,29 @@
+/*
+1. Crie a classe: Carro java e Main.java 
+2. Adicione os atributos na classe carro:
+  Ano (inteiro)
+  marca (texto)
+  modelo (texto)
+  cor (texto)
+3. Adicione os métodos na classe carro:
+exibirModelo o método deverá um exibir o modelo do carro
+exibirAnoECor() - o método deverá exibir o ano e a cor do carro
+4. O arquivo Main.java deverá conter o método main
+5. Crie um objeto carro1 com as seguintes características:
+  Ano: 1980
+  Marca: VW
+  Modelo: Fusca
+  Cor: Azul
+  Invoque os métodos exibirModelo() e exibirAnoECoro para o objeto criado (carro1)
+6. Crie um objeto com a referencia carroz com as seguintes características:
+  Ano: 2000
+  Marca: Ford
+  Modelo: Fiesta
+  Cor: Vermelho
+  Invoque os métodos exibirModelo) e exibirAnoECoro para o objeto criado (carro2)
+7. Crie um outro objeto referenciado pela variável meuCarro que carregará as informações que você escolher! (Escolha um carro qualquer e coloque as informações como dos exemplos anteriores!).
+*/
+
 class Main {
   public static void main(String[] args) {
     //objeto carro 1
