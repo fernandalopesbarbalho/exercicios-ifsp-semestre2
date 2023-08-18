7dc102d2a66bc440b95aedffce884ce39dc2bea2
Fernanda Lopes Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2023-08-18 13:21:30 -0300

    Update pessoa.java

--- pessoa.java
+++ pessoa.java
@@ -1,3 +1,15 @@
+/*
+1. Crie uma classe chamada Pessoa
+  1. Atributos : nome e idade (privados)
+  2. métodos:
+    dizerNome()
+    dizerIdade()
+    getter e setter das variáveis 
+2. No método main:
+  1. Criar um objeto Pessoa e usar todos os métodos!
+
+*/
+
 class Pessoa {
   //atributos
   private String nome;
