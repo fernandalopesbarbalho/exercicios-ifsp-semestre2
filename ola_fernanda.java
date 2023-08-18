f861608bd6c0bade0ac988d1bb0765ce23cf77bb
Fernanda Lopes Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2023-08-18 13:23:14 -0300

    Update ola_fernanda.java

--- ola_fernanda.java
+++ ola_fernanda.java
@@ -1,3 +1,8 @@
+/* A classe deve se chamar Main
+Deve ter um Método main
+Deve exibir na saída seu nome completo
+*/
+
 class Main {
   public static void main(String[] args) {
     System.out.println("Olá mundo! Sou a Fernanda Lopes Barbalho");
